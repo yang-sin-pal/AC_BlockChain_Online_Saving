f8c355560843ac94aa3370288053d0834664ecc3
Duong Tan Phat <phat.duongtan.cit22@eiu.edu.vn>
2026-07-23 16:47:40 +0700

    fix: add error messages for SavingCore to improve error handling

--- contracts/libraries/Errors.sol
+++ contracts/libraries/Errors.sol
@@ -4,3 +4,15 @@ pragma solidity ^0.8.28;
 error VaultManager_OnlySavingCore();
 error VaultManager_InsufficientBalance();
 error VaultManager_ZeroAmount();
+
+error SavingCore_PlanNotFound();
+error SavingCore_PlanNotEnabled();
+error SavingCore_DepositBelowMin();
+error SavingCore_DepositAboveMax();
+error SavingCore_ZeroAmount();
+error SavingCore_NotYetMature();
+error SavingCore_AlreadyWithdrawn();
+error SavingCore_FeeReceiverNotSet();
+error SavingCore_InvalidTenor();
+error SavingCore_InvalidApr();
+error SavingCore_InvalidDepositRange();
